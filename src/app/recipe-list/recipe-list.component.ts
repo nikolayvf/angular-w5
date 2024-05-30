082aae1f77963d8852e1647318f876ac411d3af9
nikolayvf <nikolayvf@gmail.com>
2024-05-30 22:55:20 +0300

    Added favorites functionality (Add/Remove/Show Favorites)

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -15,25 +15,40 @@ export class RecipeListComponent implements OnInit {
   public recipeInputValue = '';
   public detailCollection: any[] = [];
   public filteredCollection: any[] = [];
+  public showFavoritesOnly = false;
   private recipeApiService = inject(RecipeApiService);
 
   public ngOnInit() {
     this.recipeApiService.getAllRecipes().subscribe((data: any) => {
-      this.detailCollection = data.recipes;
+      this.detailCollection = data.recipes.map((recipe: any) => ({
+        ...recipe,
+        favorite: false,
+      }));
       this.updateFilteredCollection();
     });
   }
 
   public updateFilteredCollection() {
     const filterValue = this.recipeInputValue.toLowerCase();
-    if (filterValue === '') {
-      this.filteredCollection = this.detailCollection;
-    } else {
+    if (this.showFavoritesOnly) {
       this.filteredCollection = this.detailCollection.filter(
         (recipe) =>
-          recipe.name && recipe.name.toLowerCase().includes(filterValue)
+          recipe.favorite && recipe.name.toLowerCase().includes(filterValue)
+      );
+    } else {
+      this.filteredCollection = this.detailCollection.filter((recipe) =>
+        recipe.name.toLowerCase().includes(filterValue)
       );
     }
+  }
+
+  public toggleFavorite(recipe: any) {
+    recipe.favorite = !recipe.favorite;
+  }
+
+  public toggleShowFavorites() {
+    this.showFavoritesOnly = !this.showFavoritesOnly;
+    this.updateFilteredCollection();
   }
 
   trackById(index: number, item: any): number {
